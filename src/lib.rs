7c95f05b61a7c1bb2a5b1d15d567d43f3cfd2f10
Robert Vojta <rvojta@me.com>
2019-09-27 12:16:09 +0200

    Mark reexports as reexports in the docs
    Signed-off-by: Robert Vojta <rvojta@me.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![deny(unused_imports, unused_must_use)]
 
+#[doc(no_inline)]
 pub use crossterm_utils::{execute, queue, Command, ExecutableCommand, QueueableCommand, Result};
 
 pub use self::terminal::{terminal, Clear, ClearType, ScrollDown, ScrollUp, SetSize, Terminal};
